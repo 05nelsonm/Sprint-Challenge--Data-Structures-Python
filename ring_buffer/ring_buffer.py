7efc10434ad3cc0cc951d5a40aa356ae1c39c7f3
Matthew Nelson <05nelsonm@protonmail.com>
2020-02-28 13:06:32 -0500

    completed ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,55 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # Start things off by changing current to 0 if it is None...
+        if self.current is None:
+            self.current = 0
+
+        # If storage is full
+        if self.storage.length == self.capacity:
+
+            # Get Head
+            node = self.storage.head
+
+            if self.current > 0:
+
+                # Navigate index we're replacing
+                for _ in range(0, self.current):
+                    node = node.next
+
+            # Replace value
+            node.value = item
+
+            # If our overflow is at the end of capacity
+            if self.current == self.capacity - 1:
+                # Set back to None which'll be set to 0 on next append call
+                self.current = None
+            else:
+                # Otherwise, increment it up wards
+                self.current += 1
+
+            # Return out as to not continue with method
+            return
+
+        # Add to tail
+        self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # Get the head
+        head = self.storage.head
 
+        # While head is not none
+        while head:
+            # Add to list
+            list_buffer_contents.append(head.value)
+            # navigate to next
+            head = head.next
+
+        # Return
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
